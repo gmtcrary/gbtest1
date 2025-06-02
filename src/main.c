7fc94baf0ec30011f80a2808e638878a964ed39d
George Takumi Crary <craryg@warn.com>
2025-06-02 15:05:43 -0700

    Add cursor selection example

--- src/main.c
+++ src/main.c
@@ -3,24 +3,55 @@
 
 #include "assets/sprites/PinHeads.h"
 
-int main(void){
-  printf("\n\tPINHEADS\n\tPRESENTS:");
-  UINT8 currentspriteindex = 0;
+#define PINHEAD_LARRY 0
+#define PINHEAD_MOE   1
+#define CURSOR_SPRITE 2
+#define CURSOR_ANIM_LEN 32
 
-  set_sprite_data(0,2, PinHeads);
-  set_sprite_tile(0,0);
-  set_sprite_tile(1,1);
+int cursor_example(void){
+  set_sprite_data(0,4,PinHeads);
+  set_sprite_tile(PINHEAD_LARRY,0);
+  set_sprite_tile(PINHEAD_MOE,1);
+  set_sprite_tile(CURSOR_SPRITE,2); //3 and 4 for animation
 
-  move_sprite(0, 88, 78);
-  move_sprite(1, 80, 78);
+  uint8_t larry_x = 80;
+  uint8_t moe_x = 88;
+  const uint8_t items_y = 78;
+
+  move_sprite(PINHEAD_LARRY, larry_x, items_y); //Statically Draw Larry and Moe
+  move_sprite(PINHEAD_MOE, moe_x, items_y);
+
+  uint8_t selection[] = {larry_x, moe_x};
+  uint8_t selection_idx = 0;
+
   SHOW_SPRITES;
 
+  uint8_t anim_tick = CURSOR_ANIM_LEN;
+  uint8_t anim_idx = 0;
+
   for(;;){
-    currentspriteindex ^= 1;
-    set_sprite_tile(0,currentspriteindex);
-    set_sprite_tile(1,currentspriteindex ^ 1);
-    delay(1000);
+    move_sprite(CURSOR_SPRITE, selection[selection_idx], items_y); //Draw Cursor
+
+    vsync();
+    uint8_t joy = joypad();
+    if(joy & J_LEFT){ //&& selection_idx == 1){
+      selection_idx = 0;
+    }else if(joy & J_RIGHT){// && selection_idx == 0){
+      selection_idx = 1;
+    }
+
+    if(anim_tick == 0){
+      anim_idx ^= 1;
+      set_sprite_tile(CURSOR_SPRITE, 2 + anim_idx);
+      anim_tick = CURSOR_ANIM_LEN;
+    }
+    --anim_tick;
   }
 
+}
+
+int main(void){
+  //TODO abstract over battlestate scene
+  cursor_example();
   return 0;
 }
