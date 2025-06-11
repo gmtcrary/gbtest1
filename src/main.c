286e69ed8c994c70a96e9a0324651f6ecf72d3c8
George Takumi Crary <craryg@warn.com>
2025-06-10 23:42:21 -0700

    Move towards a board struct and add note about sprite usage pattern

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <gb/gb.h>
 
 #include "assets/sprites/PinHeads.h"
@@ -16,21 +17,26 @@ void init_setupSprites(void){
   set_sprite_tile(SPRITE_CURSOR_INDEX, 2); //2 and 3 for animation
 }
 
-typedef struct screen_entity {
+typedef struct entity {
   //TODO CURSOR SIZE FOR BIG BODY EXAMPLE
   uint8_t sprite_index;
   uint8_t screen_x;
   uint8_t screen_y;
-} screen_entity;
+} entity;
 
-void draw_game_entities(screen_entity *es, uint8_t es_len){
-  for(uint8_t i = 0; i < es_len; i++){
-    move_sprite(es[i].sprite_index, es[i].screen_x, es[i].screen_y);
+typedef struct board {
+  uint8_t count;
+  struct entity * es;
+} board;
+
+void draw_board(board * b){
+  for(uint8_t i = 0; i < b->count; i++){
+    move_sprite(b->es[i].sprite_index, b->es[i].screen_x, b->es[i].screen_y);
   }
 }
 
 typedef struct screen_cursor {
-  screen_entity * selectables;  //CHANGES TO GAMESTATE SELECTABLES REQUIRE THE CURSOR TO BE REBUILT
+  entity * selectables;  //CHANGES TO GAMESTATE SELECTABLES REQUIRE THE CURSOR TO BE REBUILT
   uint8_t selectables_len;
   uint8_t selection_idx;        //MUTABLE BY INPUT HANDLER
   uint8_t sprite_index;
@@ -67,18 +73,38 @@ void cursor_animation_handler(screen_cursor * c){
 int cursor_example(void){
   init_setupSprites();
 
-  screen_entity game_entities[] = {                       //TODO make this references to gamestate entities?
+  /*
+  entity game_entities[] = {                       //TODO make this references to gamestate entities?
     { SPRITE_PINHEAD_LARRY_INDEX, 80, 78 }
    ,{ SPRITE_PINHEAD_MOE_INDEX, 88, 78}
   };
+  */
+
+  //TODO give each entity its own sprite and init the sprite tile correctly...
+  //We can't reuse sprites
+  const uint8_t player_hand_count = 2;
+  struct entity * pb = malloc(sizeof(struct entity) * player_hand_count);
+  pb[0].sprite_index = SPRITE_PINHEAD_LARRY_INDEX;
+  pb[0].screen_x = 80;
+  pb[0].screen_y = 78;
+  pb[1].sprite_index = SPRITE_PINHEAD_MOE_INDEX;
+  pb[1].screen_x = 88;
+  pb[1].screen_y = 78;
+
+  struct board player_board = {
+    2,
+    pb
+  };
+
   const uint8_t game_entities_len = 2;
 
-  screen_cursor cursor = {{ &game_entities[0], &game_entities[1] }, 2, 0, SPRITE_CURSOR_INDEX, CURSOR_ANIM_LEN, 0 };
+  screen_cursor cursor = {{ &pb[0], &pb[1] }, 2, 0, SPRITE_CURSOR_INDEX, CURSOR_ANIM_LEN, 0 };
 
   SHOW_SPRITES;
 
   for(;;){
-    draw_game_entities(game_entities, game_entities_len);
+    //draw_game_entities(game_entities, game_entities_len);
+    draw_board(&player_board);
     draw_screen_cursor(&cursor);
 
     vsync();
@@ -87,6 +113,8 @@ int cursor_example(void){
     cursor_input_handler(&cursor, joy);
     cursor_animation_handler(&cursor);
   }
+
+  free(pb);
 
 }
 
